bafe7d9de7a63fa344be83efa682dfd075676096
kooldesigner <aries4m@gmail.com>
2017-03-27 18:01:03 +0530

    Changes by Anil Kumar.

--- Himanshu/js/app.js
+++ Himanshu/js/app.js
@@ -7,13 +7,23 @@ $scope.person = {
 
 };
 $scope.myDivShow = true;
+
 $scope.addList = [];
+
 $scope.registerSub = function(){
-	console.log($scope.person)
-	$scope.addList.push(angular.copy($scope.person));
-	$scope.person ={};
+	//console.log($scope.person)
+	if($scope.person.name=="" || $scope.person.email=="" || $scope.person.qualification==""){
+		alert("please fill it");		
+
+	}
+	else{$scope.addList.push({ 'name':$scope.person.name, 'email': $scope.person.email, 'qualification':$scope.person.qualification })
+	$scope.person={};
+};	
+	
 	
 }
+
+
 $scope.myArray = [
 {name:"Himanshu", email:'hw@gmail.com', qualification:'Graduate'},
 {name:"Anil", email:'as@gmail.com', qualification:'BTech'},
