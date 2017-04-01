e361f9b45d27a0bc296efb0445e31b646e01ee4f
anil suryavanshi <thakurinfotech@gmail.com>
2017-04-01 13:34:15 +0530

    New Code table update

--- ashu/js/app.js
+++ ashu/js/app.js
@@ -7,20 +7,39 @@ angular.module("myapp",[]).controller("mycont", function($scope, $rootScope){
 name : "",
 email : "",
 password : "",
-message : ""
+message : "",
+class:""
 };
-$scope.hi = [];
+$scope.hi = [
+	 {name:"anil", class:"12th A", email:"2",password:"",message:""},
+	 {name:"Ashu", class:"11th A", email:"1",password:"",message:""},
+	 {name:"Ravi", class:"7th A", email:"5",password:"",message:""},
+	 {name:"Himanshu", class:"BA ", email:"45",password:"",message:""}
+ ];
  $scope.regisetTeacher = function(){
-	console.log(($scope.register))
+	//console.log(($scope.register))
+	if($scope.register!=null && $scope.register.name!=null && $scope.register.class!=null && $scope.register.password!=null&& $scope.register.message!=null&& $scope.register.email!=null){
+
+	
 	$scope.hi.push(angular.copy($scope.register));
-	$scope.register = {};
+	$scope.register = {name : "",
+	email : "",
+	password : "",
+	message : "",
+	class:""};
+	//success addtions for message 
+	alert("SucessFully Added at Place- "+$scope.hi.length )
+
+	}else{
+		alert("Please fill all fields!")
+	}
 
 }
 
- $scope.myarra = [
-	 {name:"anil", class:"12th A", rollno:"2"},
-	 {name:"Ashu", class:"11th A", rollno:"1"},
-	 {name:"Ravi", class:"7th A", rollno:"5"},
-	 {name:"Himanshu", class:"BA ", rollno:"45"}
- ]
+
+$scope.removeTeacher= function(index){
+$scope.hi.splice(index,1);
+};
+
+ 
 })
