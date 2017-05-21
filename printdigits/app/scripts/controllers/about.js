3902b5e2de892f8c9526c4ebfacfa7b525f508f0
anil suryavanshi <thakurinfotech@gmail.com>
2017-05-21 22:27:38 +0530

    Print Digits Project Added

---
+++ printdigits/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name printdigtsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the printdigtsApp
+ */
+angular.module('printdigtsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ printdigits/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name printdigtsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the printdigtsApp
+ */
+angular.module('printdigtsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
