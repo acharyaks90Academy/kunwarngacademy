b851d6266e6e6460c1fa42867374c47ff16faa7b
kooldesigner <aries4m@gmail.com>
2017-06-09 02:28:43 -0700

    done by anil kumar

--- access/team/js/team.js
+++ access/team/js/team.js
@@ -26,10 +26,11 @@ app.controller("firstController", function($scope,$http,$timeout, $interval){
 		$scope.team = {
 		gender : "Male",
 		certificateNumber: Math.floor(Math.random()*519) + 1
-
 		};
 
 		
+
+		$scope.editRecord =false;
 
 
 	$scope.deleteData = function (index){	
@@ -40,22 +41,17 @@ app.controller("firstController", function($scope,$http,$timeout, $interval){
 	$timeout (function(){
 	$scope.deleteMessage = "";
 	},1500)
-};
+		};
 
-	$scope.editData = function(index){
-		//console.log($scope.myTeamJson[index]);
-		$scope.team= $scope.myTeamJson[index];
-	}
+	
 
 	$scope.pushData= function(){
 
-
-		if($scope.team.firstName == null || $scope.team.firstName == "" || $scope.team.lastName == null || $scope.team.lastName == ""
+		if(!$scope.editRecord){
+			if($scope.team.firstName == null || $scope.team.firstName == "" || $scope.team.lastName == null || $scope.team.lastName == ""
 		|| $scope.team.gender == null || $scope.team.gender == "" || $scope.team.city == null || $scope.team.city == "" ){
 			alert("haiyya... Firstname, LastName, Gender, City!!!");
-
 		}else{
-
 		$scope.myTeamJson.push({
 		'firstName':$scope.team.firstName,
 		'lastName':$scope.team.lastName,
@@ -64,25 +60,29 @@ app.controller("firstController", function($scope,$http,$timeout, $interval){
 		'gender':$scope.team.gender,
 		'city':$scope.team.city
 		})
-
 		$scope.team={
 			gender : "Male",
 			certificateNumber: Math.floor(Math.random()*519) + 1
-
 		};	
 
 		}
+		}else {
+			$scope.myTeamJson[$scope.index] = $scope.team;
+			$scope.editRecord = false;
 
+
+		}
 
 		
-
-		
-
-
 
 	};
 
 
-
+$scope.editData = function(index){
+		//console.log($scope.myTeamJson[index]);
+		$scope.team= $scope.myTeamJson[index];
+		$scope.indexHold = index;
+		$scope.editRecord =true;
+	}
 
 })
