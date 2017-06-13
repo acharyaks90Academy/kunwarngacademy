fe6b77f375abc354b07409975e5f4d8accaa46fc
kooldesigner <aries4m@gmail.com>
2017-06-13 02:46:53 -0700

    Search Page added.

---
+++ printdigits/app/scripts/controllers/searchpage.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name printdigtsApp.controller:SearchpageCtrl
+ * @description
+ * # SearchpageCtrl
+ * Controller of the printdigtsApp
+ */
+angular.module('printdigtsApp')
+  .controller('SearchpageCtrl', function ($scope, $rootScope) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+  $scope.weclome="hey";
+
+
+  $scope.searchData=[
+  {"name":"Mini Digital Swing Away Press","photo":"images/subli1.png"},
+  {"name":"Double Side Printable Machine","photo":"images/subli2.png"},
+  {"name":"Digital Swing Away Press","photo":"images/subli3.png"},
+  {"name":"American Swing Away Press","photo":"images/subli4.png"},
+  {"name":"Double Side Printable Machine","photo":"images/subli5.png"},
+  {"name":"Digital Swing Away Press","photo":"images/subli6.png"},
+  {"name":"Digital Swing Away Press","photo":"images/subli7.png"},
+  {"name":"Digital Swing Away Press","photo":"images/subli8.png"},
+  {"name":"Mini Digital Swing Away Press","photo":"images/subli1.png"},
+  {"name":"Double Side Printable Machine","photo":"images/subli2.png"},
+  {"name":"Digital Swing Away Press","photo":"images/subli3.png"},
+  {"name":"American Swing Away Press","photo":"images/subli4.png"}
+  ];
+
+
+  $scope.searchResult = $rootScope.searchResult;
+
+
+  });
