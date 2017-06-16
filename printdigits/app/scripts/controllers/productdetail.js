b8a142794e3be7d157d92a9fea12b92d9ae922da
kooldesigner <aries4m@gmail.com>
2017-06-16 01:46:51 -0700

    Product page added.

---
+++ printdigits/app/scripts/controllers/productdetail.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name printdigtsApp.controller:Heatproduct1Ctrl
+ * @description
+ * # Heatproduct1Ctrl
+ * Controller of the printdigtsApp
+ */
+angular.module('printdigtsApp')
+  .controller('productdetailCtrl', function ($scope, $rootScope, $log, $http, $location,$timeout) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+
+
+
+  $scope.noDataFound = false;
+
+  $http({
+          method: 'GET',
+          url: 'json/productdetaildata.json'
+        }).then(function successCallback(response) {
+          $scope.productData = response.data.records;        
+        }, function errorCallback(response) {
+          $scope.noDataFound = true;
+
+        });
+
+  });
