311d14d39a5d2122797ab27602f5fa653d6a763c
kooldesigner <aries4m@gmail.com>
2017-07-07 05:14:09 -0700

    Category page added.

--- printdigits/app/scripts/controllers/category.js
+++ printdigits/app/scripts/controllers/category.js
@@ -8,10 +8,17 @@
  * Controller of the printdigtsApp
  */
 angular.module('printdigtsApp')
-  .controller('CategoryCtrl', function () {
+  .controller('CategoryCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+ $scope.status = {
+    isCustomHeaderOpen: false,
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+
   });
