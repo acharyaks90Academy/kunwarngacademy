5795e4dbd8287b012054a5567a27e15a687915e8
kooldesigner <aries4m@gmail.com>
2017-07-07 02:55:22 -0700

    Category page added.

---
+++ printdigits/app/scripts/controllers/category.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name printdigtsApp.controller:CategoryCtrl
+ * @description
+ * # CategoryCtrl
+ * Controller of the printdigtsApp
+ */
+angular.module('printdigtsApp')
+  .controller('CategoryCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
